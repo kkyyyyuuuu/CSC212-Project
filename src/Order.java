a8842756b271a15600ffc9a061a5b1904ee8468b
kkyyyyuuuu <47219094+kkyyyyuuuu@users.noreply.github.com>
2025-10-25 16:43:21 +0300

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,1 +1,51 @@
+public class Order {
 
+    private int orderId;
+    private int customerReference;
+
+    private ArrayList<Integer> productIds; 
+    private double totalPrice;
+    private String orderDate;
+    private String status;
+
+    public Order(int orderId, int customerReference, ArrayList<Integer> productIds, double totalPrice, String orderDate, String status) {
+        this.orderId = orderId;
+        this.customerReference = customerReference;
+        this.productIds = productIds;
+        this.totalPrice = totalPrice;
+        this.orderDate = orderDate;
+        this.status = status;
+    }
+
+    public void cancelOrder() {
+        this.status = "canceled";
+    }
+
+    public void updateStatus(String newStatus) {
+        this.status = newStatus;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getCustomerReference() {
+        return customerReference;
+    }
+
+    public ArrayList<Integer> getProductIds() {
+        return productIds;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public String getOrderDate() {
+        return orderDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
