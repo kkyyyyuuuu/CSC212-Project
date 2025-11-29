632f0fb282ea2a34a2b59afcf66aab7d5d5c011e
Moh4mmedQ <moh4mmedaq@gmail.com>
2025-11-29 19:04:43 +0300

    Add files via upload

---
+++ src/BooleanWrapper.java
@@ -0,0 +1,9 @@
+package a;
+
+public class BooleanWrapper {
+	 private boolean v;
+	    public BooleanWrapper(boolean x){ v = x; }
+	    public boolean get(){ return v; }
+	    public void set(boolean x){ v = x; }
+
+}
