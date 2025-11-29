e9b9872c18f48e5bb1fe2382537d6825aa1b685b
MshariFTW <malmajed00@gmail.com>
2025-11-29 23:16:02 +0300

    Refactor Review class constructor and methods

--- src/Review.java
+++ src/Review.java
@@ -1,45 +1,48 @@
+package Test;
+
 public class Review {
-	  private int reviewId;
-	    private int productId;
-	    private int customerId;
-	    private int ratingScore;
-	    private String comment;
 
-	    public Review(int reviewId, int productId, int customerId, int ratingScore, String textComment) {
-	        this.reviewId = reviewId;
-	        this.productId = productId;
-	        this.customerId = customerId;
-	        this.ratingScore = ratingScore;
-	        this.comment = textComment;
-	    }
+    private int reviewId;
+    private int productId;
+    private int customerId;
+    private int ratingScore;
+    private String comment;
 
-	    public void editReview(int newRating, String newComment) {
-	        this.ratingScore = newRating;
-	        this.comment = newComment;
-	    }
+    public Review(int reviewId, int productId, int customerId, int ratingScore, String textComment) {
+        this.reviewId = reviewId;
+        this.productId = productId;
+        this.customerId = customerId;
+        this.ratingScore = ratingScore;
+        this.comment = textComment;
+    }
 
-	    public int getReviewId() {
-	        return reviewId;
-	    }
+    public void editReview(int newRating, String newComment) {
+        this.ratingScore = newRating;
+        this.comment = newComment;
+    }
 
-	    public int getProductId() {
-	        return productId;
-	    }
+    public int getReviewId() {
+        return reviewId;
+    }
 
-	    public int getCustomerId() {
-	        return customerId;
-	    }
+    public int getProductId() {
+        return productId;
+    }
 
-	    public int getRatingScore() {
-	        return ratingScore;
-	    }
+    public int getCustomerId() {
+        return customerId;
+    }
 
-	    public String getTextComment() {
-	        return comment;
-	    }
-		@Override
-		public String toString() {
-			return "Review [reviewId=" + reviewId + ", productId=" + productId + ", customerId=" + customerId
-					+ ", ratingScore=" + ratingScore + ", comment=" + comment + "]";
-		}
+    public int getRatingScore() {
+        return ratingScore;
+    }
+
+    public String getTextComment() {
+        return comment;
+    }
+	@Override
+	public String toString() {
+		return "Review [reviewId=" + reviewId + ", productId=" + productId + ", customerId=" + customerId
+				+ ", ratingScore=" + ratingScore + ", comment=" + comment + "]";
+	}
 }
