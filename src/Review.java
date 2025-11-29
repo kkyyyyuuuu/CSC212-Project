b1f67fa4bbbf3f31b1d7ca5a2aa9ac63d1633395
Moh4mmedQ <moh4mmedaq@gmail.com>
2025-11-29 19:00:19 +0300

    Update Review.java

--- src/Review.java
+++ src/Review.java
@@ -1,46 +1,45 @@
 public class Review {
+	  private int reviewId;
+	    private int productId;
+	    private int customerId;
+	    private int ratingScore;
+	    private String comment;
 
-    private int reviewId;
-    private int productId;
-    private int customerId;
-    private int ratingScore;
-    private String comment;
+	    public Review(int reviewId, int productId, int customerId, int ratingScore, String textComment) {
+	        this.reviewId = reviewId;
+	        this.productId = productId;
+	        this.customerId = customerId;
+	        this.ratingScore = ratingScore;
+	        this.comment = textComment;
+	    }
 
-    public Review(int reviewId, int productId, int customerId, int ratingScore, String textComment) {
-        this.reviewId = reviewId;
-        this.productId = productId;
-        this.customerId = customerId;
-        this.ratingScore = ratingScore;
-        this.comment = textComment;
-    }
+	    public void editReview(int newRating, String newComment) {
+	        this.ratingScore = newRating;
+	        this.comment = newComment;
+	    }
 
-    public void editReview(int newRating, String newComment) {
-        this.ratingScore = newRating;
-        this.comment = newComment;
-    }
+	    public int getReviewId() {
+	        return reviewId;
+	    }
 
-    public int getReviewId() {
-        return reviewId;
-    }
+	    public int getProductId() {
+	        return productId;
+	    }
 
-    public int getProductId() {
-        return productId;
-    }
+	    public int getCustomerId() {
+	        return customerId;
+	    }
 
-    public int getCustomerId() {
-        return customerId;
-    }
+	    public int getRatingScore() {
+	        return ratingScore;
+	    }
 
-    public int getRatingScore() {
-        return ratingScore;
-    }
-
-    public String getTextComment() {
-        return comment;
-    }
-	@Override
-	public String toString() {
-		return "Review [reviewId=" + reviewId + ", productId=" + productId + ", customerId=" + customerId
-				+ ", ratingScore=" + ratingScore + ", comment=" + comment + "]";
-	}
+	    public String getTextComment() {
+	        return comment;
+	    }
+		@Override
+		public String toString() {
+			return "Review [reviewId=" + reviewId + ", productId=" + productId + ", customerId=" + customerId
+					+ ", ratingScore=" + ratingScore + ", comment=" + comment + "]";
+		}
 }
